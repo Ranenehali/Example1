d46c41617304f2224abae7aa66a7eb1299f49585
Ranenehali <149063068+Ranenehali@users.noreply.github.com>
2023-11-01 20:53:39 +0530

    Create OneSignalSDKWorker.js

---
+++ OneSignalSDKWorker.js
@@ -0,0 +1,9 @@
+<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
+<script>
+  window.OneSignalDeferred = window.OneSignalDeferred || [];
+  OneSignalDeferred.push(function(OneSignal) {
+    OneSignal.init({
+      appId: "b29cc065-2899-48b2-bc4a-0d87ca03c036",
+    });
+  });
+</script>
